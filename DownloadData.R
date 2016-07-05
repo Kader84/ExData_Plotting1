9e81f3f7587c3a5d77b3c7f08cf8e79521d83823
Kader84 <louniskad@gmail.com>
2016-07-05 22:29:14 +0200

    Update

--- DownloadData.R
+++ DownloadData.R
@@ -1,20 +1,24 @@
 # Working directory
 
-setwd('~/Desktop/GitHub/exploratory-data-analysis')
+setwd('~/Desktop/GitHub/ExData_Plotting1')
 
-# required packages
-library(data.table)
-library(lubridate)
+
+#Download file
+
+if(!file.exists("household_power_consumption.txt")) {
+  temp <- tempfile()
+  download.file("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+  file <- unzip(temp)
+  unlink(temp)
+}
 
 # Read the file
 
-power_consumption <- read.table('./data/household_power_consumption.txt',header=TRUE,sep=';',na.strings='?')
+power_consumption <- read.table(file,header=TRUE,sep=';',na.strings='?')
 
 # Subset of data 
 
 power_consumption1 <- subset(power_consumption, power_consumption$Date=='1/2/2007' | power_consumption$Date=='2/2/2007')
-
-# Data transformation
 
 power_consumption1$Date <- as.Date(power_consumption1$Date, format="%d/%m/%Y")
 
